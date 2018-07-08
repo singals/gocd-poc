e2e040e7fa21b4f18453f9a64638116148d9d5ce
Shashank <singalshashank90@gmail.com>
2018-07-09 01:41:33 +0530

    added a dummy test

---
+++ src/test/test.js
@@ -0,0 +1,7 @@
+function dummy_test(){
+    console.log('Initializing dummy test');
+    console.log('Executing dummy test');
+    console.log('Executed dummy test successfully');
+}
+
+dummy_test();
